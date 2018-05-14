7c8636c66ea477dd43fec56b2513ad5577065c0f
stkay <stkayy@gmail.com>
2018-05-14 18:44:09 +0900

    例外処理

--- src/server/app.ts
+++ src/server/app.ts
@@ -7,28 +7,30 @@ const app: express.Express = express();
 
 app.use(express.static('public'));
 
-const port = "3000";
+const port = process.env.PORT;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port);
 
 app.get('/q', async (req, res) => {
-  res.send(await getPng(req.query.li)).status(200).end()
+  const base64 = await getPng(req.query.li).catch((err) => {
+    res.send(err.stderr).status(404).end()
+  })
+  res.send(base64).status(200).end()
 })
 
 app.get('/img/q', async (req, res) => {
-  res.send(`<img src="data:image/png;base64,${await getPng(req.query.li)}">`).status(200).end()
+  const base64 = await getPng(req.query.li).catch((err) => {
+    res.send(err.stderr).status(404).end()
+  })
+  res.send(`<img src="data:image/png;base64,${base64}">`).status(200).end()
 })
 
 const getPng = async (input) => {
   console.log(input)
   await writeFile('test.ly', input)
-  await exec('lilypond -fpng -dresolution=200 test.ly').catch((err) => {
-    //文法エラーはそのまま返す
-    console.error(err.stderr)
-    process.exit(1)
-  })
+  await exec('lilypond -fpng -dresolution=200 test.ly')
   await exec('convert test.png -trim +repage -splice 10x10 -gravity southeast -splice 10x10 out.png')
 
   const { stdout } = await exec('base64 out.png')
