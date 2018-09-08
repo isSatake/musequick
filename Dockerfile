7ceab865b0bf24534d3e8828f80dbf32f83ddff7
stkay <stkayy@gmail.com>
2018-09-08 22:48:56 +0900

    Squashed commit of the following:
    commit 5252034ecc94c8a115c9d02c5b6f28bcecd1cc0a
    Author: stkay <stkayy@gmail.com>
    Date:   Sat Sep 8 22:48:02 2018 +0900
    
        cloudinary バージョン付きURLを使用する
    
    commit df996f879a4b37781d9fd218aef51e70c143d741
    Author: stkay <stkayy@gmail.com>
    Date:   Sat Sep 8 15:30:02 2018 +0900
    
        cloudinary再設定 キャッシュが存在しない時の処理
    
    commit 8c9f20fce3635c7f4e4440fe4371512462f841d4
    Author: stkay <stkayy@gmail.com>
    Date:   Sat Sep 8 03:09:41 2018 +0900
    
        cloudinary導入

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
 FROM node:slim
 WORKDIR /workspace
-RUN apt-get update && apt-get install -y wget bzip2 imagemagick timidity lame 
+RUN apt-get update && apt-get install -y wget bzip2 timidity lame
 RUN wget http://lilypond.org/download/binaries/linux-64/lilypond-2.18.2-1.linux-64.sh && chmod u+x lilypond-2.18.2-1.linux-64.sh && ./lilypond-2.18.2-1.linux-64.sh
 COPY . /workspace
 RUN npm i
+RUN npm i -g typescript
 RUN npm run build
-ENV PORT 3000
-ENV PATH_LILYPOND lilypond
-ENV PATH_CONVERT convert
-ENV PATH_TIMIDITY timidity
-ENV PATH_LAME lame
+RUN npm uninstall -g typescript
 EXPOSE 3000
 CMD npm start
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,22 +1,25 @@
+import * as dotenv from 'dotenv';
+
+dotenv.load();
+
 import * as express from 'express';
 import * as http from 'http';
 import * as md5 from 'md5';
 import * as util from 'util';
-import * as dotenv from 'dotenv';
+import * as cloudinary from "cloudinary";
+import {unlink} from "fs";
 import {convertJPNtoEN} from "./tools";
+import {IncomingMessage} from "http";
 
-dotenv.load();
-
-const PATH_LILYPOND = process.env.PATH_LILYPOND || "lilypond";
-const PATH_CONVERT = process.env.PATH_CONVERT || "convert";
-const PATH_TIMIDITY = process.env.PATH_TIMIDITY || "timidity";
-const PATH_LAME = process.env.PATH_LAME || "lame";
 
 const writeFile = util.promisify(require('fs').writeFile);
 const exec = util.promisify(require('child_process').exec);
 const app: express.Express = express();
 
 app.use(express.static('public'));
+app.use((err, req, res, next) => {
+    return res.send(err).end(404);
+});
 
 const port = process.env.PORT;
 app.set('port', port);
@@ -24,43 +27,81 @@ app.set('port', port);
 const server = http.createServer(app);
 server.listen(port);
 
-app.get('/q', async (req, res) => {
+app.get('/q', async (req, res, next) => {
     const query = req.query.li.split(/\.(png|mp3)/);
+    const lyQuery = convertJPNtoEN(query[0]);
     const extension = query[1] || "png";
-    const fileName = md5(`${Date.now()} ${query[0]}`);
+    const fileName = md5(lyQuery);
     const tempo = req.query.tempo;
+    let url = cloudinary.url(fileName);
+    console.log(`hash: ${fileName}`, `query: ${lyQuery}`);
 
-    try {
-        if(extension === "mp3"){
-            await generateMp3(convertJPNtoEN(query[0]), fileName, tempo || 100)
-        }else{
-            await generatePng(convertJPNtoEN(query[0]), fileName)
+    if (extension === "mp3") {
+        try {
+            await generateMp3(lyQuery, fileName, tempo || 200);
+        } catch (e) {
+            next(e.stderr);
         }
-    }catch (err) {
-        res.send(err.stderr).status(404).end();
-        deleteFiles(fileName);
-        return
+        return res.sendFile(`${fileName}.mp3`, {root: __dirname}, () => {
+            deleteFiles(fileName);
+        })
     }
 
-    res.sendFile(`${fileName}.${extension}`, { root: __dirname }, () => {
-        deleteFiles(fileName)
-    })
+    const stream: IncomingMessage | void = await getPngCache(url).catch(async (err) => {
+        console.log(err.message);
+    });
+    if (stream) return stream.pipe(res);
+    try {
+        await generatePng(lyQuery, fileName);
+        url = await uploadPng(`${__dirname}/${fileName}.png`);
+    } catch (e) {
+        deleteFiles(fileName);
+        next(e.stderr);
+    }
+    deleteFiles(fileName);
+    return (await httpget(url)).pipe(res);
+});
+
+const httpget = (url: string): Promise<IncomingMessage> => new Promise(resolve => http.get(url, resolve));
+
+const getPngCache = async (url: string): Promise<IncomingMessage> => {
+    console.log("Get imgage", url);
+    const res = await httpget(url);
+    const {statusCode, statusMessage} = res;
+    if (statusCode === 404) throw new Error("Image not found");
+    if (statusCode !== 200) throw new Error(`HTTP error: ${statusCode} ${statusMessage}`);
+    console.log("Use cache");
+    return res
+};
+
+const generatePng = async (input, fileName): Promise<void> => {
+    console.log(`Generate png`);
+    await writeFile(`${__dirname}/${fileName}.ly`, `{${input}}\\header{tagline=""}`);
+    await exec(`lilypond -fpng -dresolution=200 -o ${__dirname} ${__dirname}/${fileName}.ly`);
+};
+
+const uploadPng = (path: string): Promise<string> => new Promise((resolve, reject) => {
+    cloudinary.v2.uploader.upload(path, {
+        effect: "trim", border: "10px_solid_white", use_filename: true, unique_filename: false
+    }, (err, res) => {
+        if (err) throw new Error("Failed to upload PNG");
+        console.log("Uploaded", res.url);
+        resolve(res.url);
+    });
 });
 
 const generateMp3 = async (input, fileName, tempo) => {
-    console.log(`MP3: ${input} ${fileName}.mp3`);
+    console.log(`Generate mp3`);
     await writeFile(`${__dirname}/${fileName}.ly`, `\\score{{${input}}\\midi{\\tempo4=${tempo}}}`);
-    await exec(`${PATH_LILYPOND}  -o ${__dirname} ${__dirname}/${fileName}.ly`);
-    await exec(`${PATH_TIMIDITY} ${__dirname}/${fileName}.midi -Ow -o - | ${PATH_LAME} - -b 64 ${__dirname}/${fileName}.mp3`)
+    await exec(`lilypond  -o ${__dirname} ${__dirname}/${fileName}.ly`);
+    await exec(`timidity ${__dirname}/${fileName}.midi -Ow -o - | lame - -b 64 ${__dirname}/${fileName}.mp3`)
 };
 
-const generatePng = async (input, fileName) => {
-    console.log(`PNG: ${input} ${fileName}.png`);
-    await writeFile(`${__dirname}/${fileName}.ly`, `{${input}}\\header{tagline=""}`);
-    await exec(`${PATH_LILYPOND} -fpng -dresolution=200 -o ${__dirname} ${__dirname}/${fileName}.ly`);
-    await exec(`${PATH_CONVERT} ${__dirname}/${fileName}.png -trim +repage -splice 10x10 -gravity southeast -splice 10x10 ${__dirname}/${fileName}.png`)
-};
-
-const deleteFiles = (fileName) => {
-    exec(`rm ${__dirname}/${fileName}.*`)
+const deleteFiles = (fileName): void => {
+    const path = `${__dirname}/${fileName}`;
+    const exts = ["ly", "png", "midi", "mp3"];
+    for (let ext of exts) {
+        unlink(`${path}.${ext}`, err => {
+        });
+    }
 };
